ee053017dab26c42a5fd975bbc1bd9ec871dea6c
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-04-14 16:47:27 +0530

    Time: 0 ms (100%), Space: 40.9 MB (89.91%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,24 +1,24 @@
 class Solution {
     List<List<Integer>> result;
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        result= new ArrayList<>();
-        rec(new ArrayList<>(),new HashSet<>(),n,k,1);
-        return result;    
+        result = new ArrayList<>();
+        rec(new ArrayList<>(), n, k, 1);
+        return result;
     }
-    public void rec(List<Integer> path,Set<Integer> st,int sum,int k,int idx){
-        if(sum<0) return;
-        if(path.size()==k){
-            if(sum==0) result.add(new ArrayList<>(path));
-            return ;
+
+    public void rec(List<Integer> path, int sum, int k, int idx) {
+        if (sum < 0)
+            return;
+        if (path.size() == k) {
+            if (sum == 0)
+                result.add(new ArrayList<>(path));
+            return;
         }
-        for(int i=idx;i<10;i++){
-            if(!st.contains(i)){
-                st.add(i);
-                path.add(i);
-                rec(path,st,sum-i,k,i+1);
-                st.remove(i);
-                path.remove(path.size()-1);
-            }
+        for (int i = idx; i < 10; i++) {
+            path.add(i);
+            rec(path, sum - i, k, i + 1);
+            path.remove(path.size() - 1);
         }
     }
 }
