0428674108a22b2b717b38d6ca66fc6becebfaa7
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-04-21 23:45:24 +0530

    Time: 0 ms (100%), Space: 44.9 MB (79.48%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,25 +1,28 @@
 class Solution {
     int[] visited;
+
     public boolean isBipartite(int[][] graph) {
-        visited= new int[graph.length];
+        visited = new int[graph.length];
         // st= new HashSet<>();
-        for(int i=0;i<graph.length;i++){
-            if(visited[i]==0){
-                if(!fs(graph,i,1)) return false;
+        for (int i = 0; i < graph.length; i++) {
+            if (visited[i] == 0) {
+                if (!fs(graph, i, 1))
+                    return false;
                 // count++;
             }
         }
         return true;
     }
-    public boolean fs(int[][] grid,int v,int c){
+
+    public boolean fs(int[][] grid, int v, int c) {
         // if(visited[v]!=0) return true;
-        for(int neighbour:grid[v]){
-            if(visited[neighbour]==c){
+        for (int neighbour : grid[v]) {
+            if (visited[neighbour] == c) {
                 return false;
-            }
-            else if(visited[neighbour]==0){
-            visited[neighbour]=c==1?2:1;
-            if(!fs(grid,neighbour,c==1?2:1)) return false;
+            } else if (visited[neighbour] == 0) {
+                visited[neighbour] = c == 1 ? 2 : 1;
+                if (!fs(grid, neighbour, c == 1 ? 2 : 1))
+                    return false;
             }
         }
         return true;
