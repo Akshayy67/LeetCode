7960f3a62826e48e5a1dca100d94196aa18daffd
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-04-24 16:59:35 +0530

    Time: 6 ms (34.81%), Space: 70.4 MB (84.92%) - LeetHub

---
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumPossibleSize(int[] nums) {
+        int count=0;
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1] || nums[i]<max){
+                 count++;
+                 max=Math.max(max,nums[i-1]);
+            }
+        }
+        return nums.length-count;
+    }
+}
