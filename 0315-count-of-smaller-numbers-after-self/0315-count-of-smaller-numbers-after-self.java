7f89ad77a176b9c63abc7c40d278e4295196802c
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-06-12 19:52:49 +0530

    Time: 110 ms (48.82%), Space: 59.8 MB (63.12%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,51 +1,53 @@
 class Pair{
-    int num,index,count;
-    Pair(int num,int index){
+    int num,count,idx;
+    public Pair(int num,int idx){
         this.num=num;
-        this.index=index;
+        this.idx=idx;
         count=0;
     }
 }
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        Pair[] list= new Pair[nums.length];
+        Pair[] p= new Pair[nums.length];
         for(int i=0;i<nums.length;i++){
-            list[i]= new Pair(nums[i],i);
+            p[i]= new Pair(nums[i],i);
         }
-        mergeSort(list,0,nums.length-1);
+        mergeSort(p,0,p.length-1);
+        Arrays.sort(p,(a,b)->a.idx-b.idx);
         List<Integer> result= new ArrayList<>();
-        Arrays.sort(list,(a,b)->a.index-b.index);
-        for(int i=0;i<nums.length;i++){
-            result.add(list[i].count);
-            System.out.println(list[i].num+" "+list[i].count);
+        for(int i=0;i<p.length;i++){
+            result.add(p[i].count);
         }
         return result;
     }
-    public void mergeSort(Pair[] list,int l,int r){
-        if(l<r){
-            int mid=(r-l)/2+l;
-            mergeSort(list,l,mid);
-            mergeSort(list,mid+1,r);
-            merge(list,l,mid,r);
+    public void mergeSort(Pair[] list,int low,int high){
+        if(low<high){
+            int mid=(high-low)/2+low;
+            mergeSort(list,low,mid);
+            mergeSort(list,mid+1,high);
+            merge(list,low,mid,high);
         }
     }
-    public void merge(Pair[] list,int l,int mid,int r){
-        Pair[] temp= new Pair[r-l+1];
-        int i=l,j=mid+1,k=0;
-        while(i<=mid && j<=r){
-            if(list[i].num<=list[j].num){
+    public void merge(Pair[] list,int low,int mid,int high){
+        Pair[] temp= new Pair[high-low+1];
+        int i=low,j=mid+1,k=0;
+        while(i<=mid && j<=high){
+            if(list[i].num <=list[j].num){
                 temp[k++]=list[j++];
             }
             else{
-                list[i].count+=(r-j+1);
+                list[i].count+=(high-j+1);
                 temp[k++]=list[i++];
             }
         }
-        while(i<=mid) temp[k++]=list[i++];
-        while(j<=r) temp[k++]=list[j++];
-        k=0;i=l;
-        while(i<=r){
-            list[i++]=temp[k++];
+        while(i<=mid){
+            temp[k++]=list[i++];
+        }
+        while(j<=high){
+            temp[k++]=list[j++];
+        }
+        for(i=low;i<=high;i++){
+            list[i]=temp[i-low];
         }
     }
 }
