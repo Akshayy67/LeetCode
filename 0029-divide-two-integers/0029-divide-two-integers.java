6942d531e11f1c234c1c144bf0489f4fcac91b9b
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-06-19 01:32:14 +0530

    Time: 1 ms (73.96%), Space: 41 MB (60.15%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,6 +1,21 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        long ans= (long)dividend/divisor;
-        return ans>Integer.MAX_VALUE?Integer.MAX_VALUE: ans<Integer.MIN_VALUE?Integer.MAX_VALUE:(int)ans;
+       boolean neg=false;
+        if((dividend>=0 && divisor<0) || (dividend<0 && divisor>0)) neg=true;
+        long div = Math.abs((long) dividend);
+        long dsr = Math.abs((long) divisor);
+        long ans=0;
+        while(div>=dsr){
+            long count=0;
+            while(div>=(dsr<<(count+1))){
+                count++;
+            }
+            ans+=(1L<<(count));
+            div-=(dsr<<(count));
+        }
+        if(neg) ans*=-1;
+        if(ans>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(ans<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return (int)ans;
     }
 }
