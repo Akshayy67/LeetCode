426d1aced96adcb9fa3230cc7325ce1a53676c28
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-06-27 18:56:28 +0530

    Time: 6 ms (70.44%), Space: 41.5 MB (85.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,17 +1,25 @@
 class Solution {
     public int countSubstrings(String s) {
-        int result=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(isPalin(s,i,j)) result++;
-            }
+        int i=0,count=0;
+        while(i+1<s.length()){
+            count+=palins(s,i,i+1);
+            i++;
         }
-        return result;
+        i=0;
+        while(i<s.length())
+        count+=palins(s,i,i++);
+        return count;
     }
-    public boolean isPalin(String s,int i,int j){
-        while(i<j && s.charAt(i)==s.charAt(j)){
-            i++;j--;
+    public int palins(String s,int st,int end){
+        int count=0;
+        while(st>=0 && end<s.length()){
+            if(s.charAt(st)==(s.charAt(end))){
+                st--;
+                end++;
+                count++;
+            }
+            else break;
         }
-        return i>=j;
+        return count;
     }
 }
