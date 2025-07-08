5ba9e3be4ced2224ee37c62e9adbbac5c9d81076
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-07-08 12:11:30 +0530

    Time: 52 ms (73.56%), Space: 121.5 MB (86.06%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,16 +1,27 @@
+// --- bottom up approach---//
 class Solution {
-    Integer[] nextId;
+    Integer[] nextId;  // cache for storinf next interval after curent end
     public int maxValue(int[][] events, int k) {
         nextId= new Integer[events.length];
+        // sort based on starting time
         Arrays.sort(events,(a,b)->a[0]-b[0]);
         int n=events.length;
+        // dp[i][j]=max value using at most i events and starting from index j
         int[][] dp= new int[k+1][events.length+1];
+        //fill dp table
+        //i -> number of events allowed (1->k)
+        //j -> cur index from right to left
         for(int i=1;i<=k;i++){
             for(int j=n-1;j>=0;j--){
+                // find the next evetnt index start after event[j] ends
                 int next=bs(events,j+1,events[j][1]);
+                //two choices
+                //   1.skip current -> d[i][j+1];
+                //   2.take current event-> cur event profit +dp[i-1][next]
                 dp[i][j]=Math.max(dp[i][j+1],events[j][2]+dp[i-1][next]);
             }
         }
+        // max value using at most k events starting from index 0;
         return dp[k][0];
     }
     public int bs(int[][] events,int i,int target){
@@ -24,28 +35,39 @@ class Solution {
         return nextId[i-1]=low;
     }
 }
-// class Solution {
-//     Integer[][] dp;
-//     public int maxValue(int[][] events, int k) {
-//         dp= new Integer[k+1][events.length];
-//         Arrays.sort(events,(a,b)->a[0]-b[0]);
-//         return helper(events,k,0);
-//     }
-//     public int helper(int[][] events,int k,int i){
-//         if(i>=events.length) return 0;
-//         if(k==0) return 0;
-//         if(dp[k][i]!=null) return dp[k][i];
-//         int leave=helper(events,k,i+1);
-//         int low=i+1,high=events.length-1,mid=0;
-//         while(low<=high){
-//             mid=(low+high)/2;
-//             if(events[mid][0]<=events[i][1]){
-//                 low=mid+1;
-//             }
-//             else high=mid-1;
-//         }
-//         int take=events[i][2]+helper(events,k-1,low);
-//         return dp[k][i]=Math.max(take,leave);
+    // -- Top down Approach--
+class Solutio {
+    Integer[][] dp;
+    public int maxValue(int[][] events, int k) {
+        dp= new Integer[k+1][events.length];
+        // sort based on starting time
+        Arrays.sort(events,(a,b)->a[0]-b[0]);
+        // recursion from first event and k remaining picks
+        return helper(events,k,0);
+    }
+    public int helper(int[][] events,int k,int i){
+        // no more events
+        if(i>=events.length) return 0;
+        // no picks left
+        if(k==0) return 0;
+        //memoization check
+        if(dp[k][i]!=null) return dp[k][i];
+        //skipppppp
+        int leave=helper(events,k,i+1);
+        //take cur value
+        // use binary search to find next non-overlapping event
+        int low=i+1,high=events.length-1,mid=0;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(events[mid][0]<=events[i][1]){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        // now low points to the first event that starts after events[i] ends
+        int take=events[i][2]+helper(events,k-1,low);
+        //memoize
+        return dp[k][i]=Math.max(take,leave);
         
-//     }
-// }
+    }
+}
