90d52889ffc2795e0af4651c655ad373c7df09ac
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-07-23 20:03:26 +0530

    Time: 134 ms (50%), Space: 45.8 MB (61.64%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        StringBuilder str= new StringBuilder(s);
+        boolean changes=true;
+        int score=0;
+        char first=x>y?'b':'a';
+        int firstscore=x>y?x:y;
+        int secondscore=x>y?y:x;
+        char second=first=='a'?'b':'a';
+        boolean totalchanges=true;
+        while(totalchanges){
+            totalchanges=false;
+            changes=true;
+            while(changes && str.length()>=2){
+                changes=false;
+                StringBuilder temp= new StringBuilder();
+                temp.append(str.charAt(0));
+                for(int i=1;i<str.length();i++){
+                    if(str.charAt(i)==first){
+                        if(!temp.isEmpty() && temp.charAt(temp.length()-1)==second){
+                            temp.deleteCharAt(temp.length()-1);
+                            score+=firstscore;
+                            changes=true;
+                            totalchanges=true;
+                            continue;
+                        }
+                    }
+                    temp.append(str.charAt(i));
+                }
+                str=temp;
+            }
+            changes=true;
+            while(changes && str.length()>=2){
+                changes=false;
+                StringBuilder temp= new StringBuilder();
+                temp.append(str.charAt(0));
+                for(int i=1;i<str.length();i++){
+                    if(str.charAt(i)==second){
+                        if(!temp.isEmpty() && temp.charAt(temp.length()-1)==first){
+                            temp.deleteCharAt(temp.length()-1);
+                            score+=secondscore;
+                            changes=true;
+                            totalchanges=true;
+                            continue;
+                        }
+                    }
+                    temp.append(str.charAt(i));
+                }
+                str=temp;
+            }
+        }
+        return score;
+    }
+}
