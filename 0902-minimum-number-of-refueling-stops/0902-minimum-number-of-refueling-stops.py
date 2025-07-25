80b4e15f344bec23fe47ca35151e15990993d43c
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-07-25 23:55:07 +0530

    Time: 4 ms (42.31%), Space: 18.2 MB (35.83%) - LeetHub

--- 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.py
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.py
@@ -1,18 +1,17 @@
 import heapq
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        pq=[]
         result=0
+        pq=[]
         i=0
         fuel=startFuel
-        while fuel<target:
-            while i<len(stations) and fuel>=stations[i][0]:
+        while(fuel<target):
+            while(i<len(stations) and fuel>=stations[i][0]):
                 heapq.heappush(pq,-stations[i][1])
                 i+=1
-            if len(pq)==0:
+            if(len(pq)==0):
                 return -1
             result+=1
             fuel+=-heapq.heappop(pq)
         return result
 
-        
