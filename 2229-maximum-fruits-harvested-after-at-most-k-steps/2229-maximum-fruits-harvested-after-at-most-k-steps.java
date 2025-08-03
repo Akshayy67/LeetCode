bc942cc74ab86c8959385aae7d4c16e5f3edc264
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-08-03 12:31:00 +0530

    Time: 2 ms (97.47%), Space: 98.2 MB (48.1%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
-        int result=0,sum=0,j=0;
+        int j=0,result=0,sum=0;
         for(int i=0;i<fruits.length;i++){
             sum+=fruits[i][1];
-            while(j<=i && !possible(fruits[j][0],fruits[i][0],startPos,k)) sum-=fruits[j++][1];
-            result= Math.max(result,sum);
+            while(j<=i && !isPossible(fruits[i][0],fruits[j][0],startPos,k)) sum-=fruits[j++][1];
+            result=Math.max(result,sum);
         }
         return result;
     }
-    public boolean possible(int j,int i,int pos,int k){
-        int min=Math.min(Math.abs(i-pos),Math.abs(j-pos))+(i-j);
+    public boolean isPossible(int i,int j,int sp,int k){
+        int min=Math.min(Math.abs(sp-i),Math.abs(sp-j))+(i-j);
         return min<=k;
     }
 }
