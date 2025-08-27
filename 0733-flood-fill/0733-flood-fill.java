8b6c55c9a3dba2ad1d60f44f105a64b8307d4089
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-08-27 13:49:15 +0530

    Time: 1 ms (45.09%), Space: 44.9 MB (84.38%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        fill(image,sr,sc,image[sr][sc]);
+        image[sr][sc]=-1;
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image[0].length;j++){
+                if(image[i][j]==-1) image[i][j]=color;
+            }
+        }
+        return image;
+    }
+    public void fill(int[][] image, int i,int j,int color){
+        for(int[] dir:directions){
+            int ni=dir[0]+i,nj=dir[1]+j;
+            if(valid(ni,nj,image) && image[ni][nj]==color){
+                image[ni][nj]=-1;
+                fill(image,ni,nj,color);
+            }
+        }
+    }
+    public boolean valid(int i, int j,int[][] grid){
+        return i>=0 && j>=0 && i<grid.length && j<grid[0].length;
+    }
+}
