de872a9c8228faa6ed6488ab0f8e867f6f141b91
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-08-31 11:35:31 +0530

    Time: 1 ms (99.87%), Space: 44.2 MB (89.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,26 +1,42 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Character>[] rows= new HashSet[9];
-        Set<Character>[] cols= new HashSet[9];
-        Set<Character>[][] dabba= new HashSet[3][3];
-        for(int i=0;i<9;i++){
-            rows[i]=new HashSet<>();
-            cols[i]=new HashSet<>();
-        }
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                dabba[i][j]= new HashSet<>();
-            }
-        }
+        // Set<Character>[] rows= new HashSet[9];
+        // Set<Character>[] cols= new HashSet[9];
+        // Set<Character>[][] dabba= new HashSet[3][3];
+        // for(int i=0;i<9;i++){
+        //     rows[i]=new HashSet<>();
+        //     cols[i]=new HashSet<>();
+        // }
+        // for(int i=0;i<3;i++){
+        //     for(int j=0;j<3;j++){
+        //         dabba[i][j]= new HashSet<>();
+        //     }
+        // }
+        // for(int i=0;i<9;i++){
+        //     for(int j=0;j<9;j++){
+        //         if(board[i][j]=='.') continue;
+        //         if(rows[i].contains(board[i][j])) return false;
+        //         if(cols[j].contains(board[i][j])) return false;
+        //         if(dabba[i/3][j/3].contains(board[i][j])) return false;
+        //         rows[i].add(board[i][j]);
+        //         cols[j].add(board[i][j]);
+        //         dabba[i/3][j/3].add(board[i][j]);
+        //     }
+        // }
+        // return true;
+        int[] rows= new int[9];
+        int[] cols= new int[9];
+        int[] dabba= new int[9];
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
-                if(rows[i].contains(board[i][j])) return false;
-                if(cols[j].contains(board[i][j])) return false;
-                if(dabba[i/3][j/3].contains(board[i][j])) return false;
-                rows[i].add(board[i][j]);
-                cols[j].add(board[i][j]);
-                dabba[i/3][j/3].add(board[i][j]);
+                int num=board[i][j]-'1';
+                int v=1<<num;
+                int dabbaidx=(i/3)*3+j/3;
+                if((rows[i]&v)!=0 || (cols[j]&v)!=0 || (dabba[dabbaidx]&v)!=0) return false;
+                rows[i]|=v;
+                cols[j]|=v;
+                dabba[dabbaidx]|=v;
             }
         }
         return true;
