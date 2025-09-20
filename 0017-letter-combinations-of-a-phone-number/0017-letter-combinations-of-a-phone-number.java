25aaff51a98c0f8e430b94f9a970244d0ff13587
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-09-20 15:40:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.48%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -3,23 +3,22 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0) return new ArrayList<>();
         result= new ArrayList<>();
-        rec(digits,0,new StringBuilder());
+        helper(digits,0,new StringBuilder());
         return result;
     }
-    public void rec(String digits,int i,StringBuilder path){
+    public void helper(String digits, int i,StringBuilder path){
         if(i==digits.length()){
             result.add(path.toString());
             return;
         }
-        int dial=digits.charAt(i)-'0';
-        char c=((char) ('a'+3*(dial-2)));
-        if(dial>7) c++;
-        int n=3;
-        if(dial==7 || dial==9) n=4;
-        for(int idx=0;idx<n;idx++){
-            path.append((char)(c+idx));
-            rec(digits,i+1,path);
+        int num=digits.charAt(i)-'2';
+        int start=(num>5)?3*num+1:3*num;
+        int end=(num==5 || num==7)?4:3;
+        for(int idx=0;idx<end;idx++){
+            path.append((char)(start+'a'));
+            helper(digits,i+1,path);
             path.setLength(path.length()-1);
+            start++;
         }
     }
 }
