98d8c1ef9bb78a3feff049016530a92d47c71c8d
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-09 10:41:08 +0530

    Time: 4 ms (55.55%), Space: 45.9 MB (13.26%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,24 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int one=0,two=0;
-        for(int num:nums){
-            one=(one^num)&~two;
-            two=(two^num)&~one;
+        int result=0;
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(int num:nums){
+                if((num&(1<<i))!=0) count++;
+            }
+            if(count%3!=0) result|=(1<<i);
         }
-        return one;
+        return result;
     }
 }
-
-// class Solution {
-//     public int singleNumber(int[] nums) {
-//         int result=0;
-//         for(int i=0;i<32;i++){
-//             int cnt=0;
-//             for(int num:nums){
-//                 cnt+=((num>>i)&1);
-//             }
-//             if(cnt%3!=0) result|=(1L<<i);
-//         }
-//         return result;
-//     }
-// }
