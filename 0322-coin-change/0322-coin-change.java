dbd663366e46ad90f7e11f1d65b28040f83e7bab
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-14 23:49:23 +0530

    Time: 35 ms (14.28%), Space: 45.5 MB (18.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,18 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int coinChange(int[] coins, int amount) {
-        dp= new Integer[amount+1][coins.length+1];
-        int ans=helper(coins,amount,0);
-        return ans>=(int)1e7?-1:ans;
+        dp= new int[coins.length][amount+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        int result=helper(coins,amount,0);
+        if(result>(int)1e5) return -1;
+        return result;
     }
     public int helper(int[] coins,int amount,int i){
         if(amount==0) return 0;
-        if(i==coins.length) return (int) 1e7;
-        if(dp[amount][i]!=null) return dp[amount][i];
+        if(i==coins.length || amount<0) return (int) 1e6;
+        if(dp[i][amount]!=-1) return dp[i][amount];
         int leave=helper(coins,amount,i+1);
-        int take=(int)1e7;
-        if(amount-coins[i]>=0) take=1+helper(coins,amount-coins[i],i);
-        return dp[amount][i]=Math.min(take,leave);
+        int take=1+helper(coins,amount-coins[i],i);
+        return dp[i][amount]=Math.min(leave,take);
     }
 }
