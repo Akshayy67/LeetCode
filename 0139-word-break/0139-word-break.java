15b8534aba97d2257fc214264c0ed27bd6966d49
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-14 22:33:45 +0530

    Time: 1 ms (99.35%), Space: 42.5 MB (72.84%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,46 +1,43 @@
+
 class TNode{
     TNode[] children= new TNode[26];
-    boolean isEOW;
+    boolean isEow;
 }
-
 class Trie{
-    TNode root=new TNode();
+    TNode root= new TNode();
+    Boolean[] dp;
     public void insert(String s){
         TNode cur=root;
         for(char c:s.toCharArray()){
-            if(cur.children[c-'a']==null) cur.children[c-'a']=new TNode();
-            cur=cur.children[c-'a'];
-        }
-        cur.isEOW=true;
-    }
-    public boolean canForm(String s,int index,Map<Integer,Boolean> dp){
-        if(dp.containsKey(index)) return dp.get(index);
-        TNode cur=root;
-        for(int i=index;i<s.length();i++){
-            int idx=s.charAt(i)-'a';
-            if(cur.children[idx]==null) break;
+            int idx=c-'a';
+            if(cur.children[idx]==null) cur.children[idx]= new TNode();
             cur=cur.children[idx];
-            if(cur.isEOW){
-                if(i==s.length()-1) return true;
-                if(canForm(s,i+1,dp)) {
-                    dp.put(index,true);
-                    return true;
-                }
-            }
-        }  
-        dp.put(index,false);
-        return false;
+        }
+        cur.isEow=true;
+    }
+    public boolean search(String s){
+        dp= new Boolean[s.length()];
+        return helper(s,0,root);
+
+    }
+    public boolean helper(String s,int i,TNode cur){
+        if(i==s.length()) return false;
+        if(dp[i]!=null) return dp[i];
+        TNode now=cur;
+        for(int j=i;j<s.length();j++){
+            int idx=s.charAt(j)-'a';
+            if(now.children[idx]==null) return dp[i]=false;
+            now=now.children[idx];
+            if(j+1==s.length() && now.isEow) return true;
+            if(now.isEow) if(helper(s,j+1,root)) return true;
+        }
+        return dp[i]=false;
     }
 }
-
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Trie trie= new Trie();
-        for(String str:wordDict){
-            // if(!str.isEmpty() && trie.canForm(str,0)) continue;
-            if(!str.isEmpty())
-            trie.insert(str);
-        } 
-        return trie.canForm(s,0,new HashMap<>());
+        for(String word:wordDict) trie.insert(word);
+        return trie.search(s);
     }
 }
