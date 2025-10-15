d2a290c91743ac9807e6c493a8c371f05323393b
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-15 12:15:53 +0530

    Time: 4 ms (59.72%), Space: 48.2 MB (8.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(i==0 && j==0) continue;
-                grid[i][j]=grid[i][j]+Math.min((i-1>=0)?grid[i-1][j]:Integer.MAX_VALUE,(j-1>=0)?grid[i][j-1]:Integer.MAX_VALUE);
+        for(int i=1;i<grid.length;i++) grid[i][0]=grid[i-1][0]+grid[i][0];
+        for(int i=1;i<grid[0].length;i++) grid[0][i]=grid[0][i-1]+grid[0][i];
+        for(int i=1;i<grid.length;i++){
+            for(int j=1;j<grid[0].length;j++){
+                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
         return grid[grid.length-1][grid[0].length-1];
