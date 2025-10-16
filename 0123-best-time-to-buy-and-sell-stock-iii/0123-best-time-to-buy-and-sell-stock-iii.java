22044f952642b5bab14f5f6a7c28c0976cc6d9c8
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-16 12:30:05 +0530

    Time: 100 ms (25.11%), Space: 76.3 MB (24.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,14 +1,18 @@
 class Solution {
     int[] prices;
-    Integer[][][] dp;
+    int[][][] dp;
     public int maxProfit(int[] prices) {
         this.prices=prices;
-        dp= new Integer[prices.length][2][3];
+        dp= new int[prices.length][2][3];
+         for (int i = 0; i < prices.length; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++)
+                    dp[i][j][k] = -1;
         return helper(0,1,0);
     }
     public int helper(int i,int buy,int transaction){
         if(transaction==2 || i==prices.length) return 0;
-        if(dp[i][buy][transaction]!=null) return dp[i][buy][transaction];
+        if(dp[i][buy][transaction]!=-1) return dp[i][buy][transaction];
         int leave=helper(i+1,buy,transaction);
         int take=0;
         if(buy==1){
