1f478af9df056cba53f9aced12778610d3be8548
Akshayy67 <153260857+Akshayy67@users.noreply.github.com>
2025-10-25 14:48:39 +0530

    Time: 15 ms (99.65%), Space: 45.4 MB (91.52%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
@@ -1,19 +1,19 @@
 class Solution {
-    long[][][] dp;
     public long numberOfWays(String s) {
-        dp= new long[s.length()+1][4][3];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[i].length;j++){
-                for(int k=0;k<dp[i][j].length;k++) dp[i][j][k]=-1;
+        int zero=0,one=0;
+        long oneZero=0,zeroOne=0,total=0;
+        for(char c:s.toCharArray()){
+            if(c=='0'){
+                zero++;
+                oneZero+=one;  // each prev one can pair up to zero to form onezero"10"
+                total+=zeroOne;
+            }
+            else{
+                one++;
+                zeroOne+=zero;  // each prev zero can pair up to one to form onezero"10"
+                total+=oneZero;
             }
         }
-        return helper(s,0,3,-1);
-    }
-    public long helper(String s,int i,int count,int prev){
-        if(count==0) return 1;
-        if(i==s.length()) return 0;
-        if(dp[i][count][prev+1]!=-1) return dp[i][count][prev+1];
-        if(s.charAt(i)-'0'==prev) return dp[i][count][prev+1]= helper(s,i+1,count,prev);
-        else return dp[i][count][prev+1]=  helper(s,i+1,count-1,s.charAt(i)-'0')+helper(s,i+1,count,prev);
+        return total;
     }
 }
